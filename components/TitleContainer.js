ce94a7d417a2cb5d4c8241e7a13b4edef1b2ddaa
Franco Juarez <franjuaache@gmail.com>
2024-08-09 14:44:01 -0300

    Update TitleContainer.js

--- components/TitleContainer.js
+++ components/TitleContainer.js
@@ -3,7 +3,7 @@ import styles from "../styles/navBar.module.css";
 export default function TitleContainer() {
   return (
     <div className={styles.titleContainer}>
-      <h1>Una guía para la imaginación política</h1>
+      <h1>Un blog de tecnología</h1>
       <h5>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit.
         <br /> Dignissimos maiores molestiae temporibus illo expedita nostrum.
